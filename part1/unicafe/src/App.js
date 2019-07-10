53630a74f7241ddf7102d50196232ee471acba48
Mesfin Tegegne <mesfint@Mesfins-MBP.home>
2019-07-10 19:20:41 +0300

    1.6 unicafe step1 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  //save clicks of each button
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+const goodHandleClick = () => {
+  return setGood(good + 1);
+}
+const neutralHandleClick = () => {
+  return setNeutral(neutral + 1);
+}
+const badHandleClick = () => {
+  return setBad(bad + 1);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Give FeedBack!</h2>
+     
+    <button className="good"  onClick={goodHandleClick}>Good </button>
+    
+    <button className="neutral"  onClick={neutralHandleClick}>Neutral </button>
+   
+    <button className="bad"  onClick={badHandleClick}> Bad</button>
+     <hr />
+     
+     good:<span className="g">{good}</span>{' '}
+     neutral:<span className='n'>{neutral}</span>{ ' '}
+     bad:<span className='b'> {bad}</span>
+      
+      
     </div>
   );
 }
