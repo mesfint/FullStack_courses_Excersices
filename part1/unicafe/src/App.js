c28a6a0ac7b5f7888c6a78336ed68638c449cd15
Mesfin Tegegne <mesfint@Mesfins-MBP.home>
2019-07-10 22:45:20 +0300

    1.7_unicafe_step2_completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,42 +1,71 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   //save clicks of each button
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [result, setResult] = useState(0);
 
-const goodHandleClick = () => {
-  return setGood(good + 1);
-}
-const neutralHandleClick = () => {
-  return setNeutral(neutral + 1);
-}
-const badHandleClick = () => {
-  return setBad(bad + 1);
-}
+  const goodHandleClick = () => {
+    return setGood(good + 1);
+  };
+  const neutralHandleClick = () => {
+    return setNeutral(neutral + 1);
+  };
+  const badHandleClick = () => {
+    return setBad(bad + 1);
+  };
+
+  const totalHandleClick = () => {
+    let g = setGood({ good });
+    let n = setNeutral({ neutral });
+    let b = setBad({ bad });
+    setResult({ result: g + n + b });
+  };
+  // const averageHandleClick = () =>{
+  //   let g =  setGood({good:good});
+  //     let n = setNeutral({neutral:neutral});
+  //     let b = setBad({bad:bad});
+  //   let sum = setResult({result: g + n + b});
+
+  // }
 
   return (
     <div className="App">
-    <h2>Give FeedBack!</h2>
-     
-    <button className="good"  onClick={goodHandleClick}>Good </button>
-    
-    <button className="neutral"  onClick={neutralHandleClick}>Neutral </button>
-   
-    <button className="bad"  onClick={badHandleClick}> Bad</button>
-     <hr />
-     
-     good:<span className="g">{good}</span>{' '}
-     neutral:<span className='n'>{neutral}</span>{ ' '}
-     bad:<span className='b'> {bad}</span>
-      
-      
+      <h2>Give FeedBack!</h2>
+      <button className="good" onClick={goodHandleClick}>
+        Good{" "}
+      </button>
+      <button className="neutral" onClick={neutralHandleClick}>
+        Neutral{" "}
+      </button>
+      <button className="bad" onClick={badHandleClick}>
+        {" "}
+        Bad
+      </button>
+      <hr />
+      good:<span className="g">{good}</span> neutral:
+      <span className="n">{neutral}</span> bad:<span className="b"> {bad}</span>
+      <span className="n">result:{good + neutral + bad}</span>
+      <span className="n">
+        <br />
+        avarege:
+        {typeof parseFloat(
+          (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
+        ) === NaN
+          ? 0
+          : (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
+      </span>
+      <br />
+      <span className="g">
+        Positive:{parseFloat((good / (good + neutral + bad)) * 100)}%
+      </span>
     </div>
   );
-}
+};
 
 export default App;
