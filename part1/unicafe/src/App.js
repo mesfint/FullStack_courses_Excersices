f48036c03fe624bf96cc0b6c3fb7814263ca4e7a
Mesfin Tegegne <mesfint@Mesfins-MBP.home>
2019-07-10 23:02:17 +0300

    1.8_Unicafe_step3_completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,6 +3,29 @@ import ReactDOM from "react-dom";
 
 import "./App.css";
 
+const Statistics = ({ bad, good, neutral }) => {
+  return (
+    <div>
+      good:<span className="g">{good}</span> neutral:
+      <span className="n">{neutral}</span> bad:<span className="b"> {bad}</span>{" "}
+      <span className="n">result: {good + neutral + bad}</span>
+      <span className="n">
+        <br />
+        avarege:
+        {typeof parseFloat(
+          (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
+        ) === NaN
+          ? 0
+          : (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
+      </span>
+      <br />
+      <span className="g">
+        Positive:{parseFloat((good / (good + neutral + bad)) * 100)}%
+      </span>
+    </div>
+  );
+};
+
 const App = () => {
   //save clicks of each button
   const [good, setGood] = useState(0);
@@ -20,20 +43,6 @@ const App = () => {
     return setBad(bad + 1);
   };
 
-  const totalHandleClick = () => {
-    let g = setGood({ good });
-    let n = setNeutral({ neutral });
-    let b = setBad({ bad });
-    setResult({ result: g + n + b });
-  };
-  // const averageHandleClick = () =>{
-  //   let g =  setGood({good:good});
-  //     let n = setNeutral({neutral:neutral});
-  //     let b = setBad({bad:bad});
-  //   let sum = setResult({result: g + n + b});
-
-  // }
-
   return (
     <div className="App">
       <h2>Give FeedBack!</h2>
@@ -48,22 +57,7 @@ const App = () => {
         Bad
       </button>
       <hr />
-      good:<span className="g">{good}</span> neutral:
-      <span className="n">{neutral}</span> bad:<span className="b"> {bad}</span>
-      <span className="n">result:{good + neutral + bad}</span>
-      <span className="n">
-        <br />
-        avarege:
-        {typeof parseFloat(
-          (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
-        ) === NaN
-          ? 0
-          : (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
-      </span>
-      <br />
-      <span className="g">
-        Positive:{parseFloat((good / (good + neutral + bad)) * 100)}%
-      </span>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
