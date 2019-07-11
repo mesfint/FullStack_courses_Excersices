ff14429c62426b6db67c0acddf2ec60b03cd6afa
Mesfin Tegegne <mesfint@Mesfins-MBP.home>
2019-07-11 23:48:01 +0300

    part1_1.11_unicafe_step6_completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState  } from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
 
-function App() {
+
+
+const  App = (props) => {
+  const [selected, setSelected] = useState();
+  //const name = prompt(`Please name`);
+  //Update state
+  //setSelected();
+
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  
+
+  const getRandomQoute = () =>{
+    //console.log('You wish');
+  
+  let i = (Math.round(Math.random() * 6) +1);
+  const randomQuote = anecdotes[i];
+      return  setSelected(randomQuote);
+
+  
+  //console.log(randomQuote);
+   
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <span className="quotes">{selected}</span>
+      <br />
+      <button
+      
+      onClick={getRandomQoute}
+      >
+        Next Anecdotes
+      </button>
+
     </div>
   );
 }
+
 
 export default App;
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,13 +7,15 @@ const Statistic = ({ text, value, className }) => {
   if (text === "positive") {
     return (
       <div>
-        {text}:<span className={className}>{value} %</span>
+        {text}
+        <span className={className}>{value} %</span>
       </div>
     );
   } else {
     return (
       <div>
-        {text}:<span className={className}>{value}</span>
+        {text}
+        <span className={className}>{value}</span>
       </div>
     );
   }
@@ -21,13 +23,44 @@ const Statistic = ({ text, value, className }) => {
 const Statistics = props => {
   return (
     <div>
-      <Statistic className="g" text="good" value={props.good} />
-      <Statistic className="b" text="bad" value={props.bad} />
-      <Statistic className="n" text="neutral" value={props.neutral} />
-      <Statistic className="n" text="total" value={props.total} />
-
-      <Statistic className="b" text="average" value={props.average} />
-      <Statistic className="b" text="positive" value={props.positive} />
+      <table className="table">
+        <tr>
+          <td>Good</td>
+          <td>
+            <Statistic className="g" value={props.good} />
+          </td>
+        </tr>
+        <tr>
+          <td>Bad</td>
+          <td>
+            <Statistic className="b" value={props.bad} />
+          </td>
+        </tr>
+        <tr>
+          <td>Neutral</td>
+          <td>
+            <Statistic className="n" value={props.neutral} />
+          </td>
+        </tr>
+        <tr>
+          <td>Total</td>
+          <td>
+            <Statistic className="n" value={props.total} />
+          </td>
+        </tr>
+        <tr>
+          <td>Average</td>
+          <td>
+            <Statistic className="b" value={props.average} />
+          </td>
+        </tr>
+        <tr>
+          <td>Positive</td>
+          <td>
+            <Statistic className="b" value={props.positive} />%
+          </td>
+        </tr>
+      </table>
     </div>
   );
 };
@@ -37,7 +70,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [result, setResult] = useState(0);
+  //const [result, setResult] = useState(0);
 
   const goodHandleClick = () => {
     return setGood(good + 1);
@@ -70,8 +103,8 @@ const App = () => {
     <div className="App">
       <h2>Give FeedBack!</h2>
       <Button onClick={goodHandleClick} text="good" />
-      <Button onClick={neutralHandleClick} text="neutral" />
       <Button onClick={badHandleClick} text="bad" />
+      <Button onClick={neutralHandleClick} text="neutral" />
 
       <hr />
       <h2>Statistics</h2>
