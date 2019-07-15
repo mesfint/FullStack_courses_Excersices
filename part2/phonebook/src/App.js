107c53f96de9c8c86f8ea491b531cfcf02bbb9c4
Mesfin Tegegne <mesfin_meng@yahoo.com>
2019-07-15 21:54:46 +0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
+const App = (props) => {
   const [persons, setPersons] = useState([
     { name: "Mesfin Tegegne ",number: '045-123456' },
     { name: "Timiro Ali ",number: '046-234567' },
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Persons = () =>{
+
+}
+
+
+
+
+export default Persons;
