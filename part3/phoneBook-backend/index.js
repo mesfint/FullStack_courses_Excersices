f3f7a7b4eef52c19411afb9aae189223deacba53
Mesfin Tegegne <mesfin_meng@yahoo.com>
2021-04-15 00:15:14 +0300

    3.2 completed

--- part3/phoneBook-backend/index.js
+++ part3/phoneBook-backend/index.js
@@ -19,6 +19,10 @@ let persons = [
     number: '39-23-3456666',
   },
 ];
+const person = {
+  time: new Date(),
+  length: Object.keys(persons).length,
+};
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
@@ -27,6 +31,16 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
+app.get('/info', (request, response) => {
+  response.send(
+    'Phonebook has info for  ' +
+      person.length +
+      ' people' +
+      '<br />' +
+      person.time
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
