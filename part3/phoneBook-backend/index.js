7b8179df35b1f11bc22f6bdb45a0d71a4b55673d
Mesfin Tegegne <mesfin_meng@yahoo.com>
2021-04-15 08:27:35 +0300

    3.4 completed

--- part3/phoneBook-backend/index.js
+++ part3/phoneBook-backend/index.js
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end();
   }
 });
+//Delete single entry
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
 
 app.get('/info', (request, response) => {
   response.send(
