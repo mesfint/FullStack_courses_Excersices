3cbb168b16c24461eb2e45aead9284631f11016d
Mesfin Tegegne <mesfin_meng@yahoo.com>
2021-05-26 19:34:23 +0300

    update

--- note-app-Backend/index.js
+++ note-app-Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('build'));
+app.use(express.json());
 
 let notes = [
   {
@@ -22,12 +22,6 @@ let notes = [
     date: '2020-01-10T19:20:14.298Z',
     important: true,
   },
-  {
-    id: 4,
-    content: 'Deploy app to the internet',
-    date: '2020-01-10T18:39:34.091Z',
-    important: true,
-  },
 ];
 
 app.get('/', (req, res) => {
@@ -36,6 +30,32 @@ app.get('/', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
   res.json(notes);
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
 });
 
 app.get('/api/notes/:id', (request, response) => {
@@ -49,7 +69,14 @@ app.get('/api/notes/:id', (request, response) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
