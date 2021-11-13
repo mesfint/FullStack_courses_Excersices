77d002415f6fd502669c7bdb748f56f918fe3a33
mesfint <mesfin_meng@yahoo.com>
2021-11-14 00:01:48 +0200

    phonebackend and frontend mixed

---
+++ part3/phoneBook-backend/build/precache-manifest.8e560a18c3d2009ae19e04abf3cce53e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b617d13902f15d4487f43caa79c8b642",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99c60ac803e047672958",
+    "url": "/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "2c8abd1a399a94d17497",
+    "url": "/static/css/main.b6fa1212.chunk.css"
+  },
+  {
+    "revision": "99c60ac803e047672958",
+    "url": "/static/js/2.23e1a796.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.23e1a796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8abd1a399a94d17497",
+    "url": "/static/js/main.28334c94.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
